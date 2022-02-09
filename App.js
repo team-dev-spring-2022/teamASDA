781babc9be4b157734fe6be666353de889539d89
SomeoneUnknownPerson <sema.shamaev1@gmail.com>
2022-02-10 01:17:21 +0900

    #2 Добавлен стартовый шаблон

---
+++ App.js
@@ -0,0 +1,14 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+const Stack = createNativeStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+    </NavigationContainer>
+  );
+}

c487b51d8f4c5b23c7a0b80133ba93cf4ff64e4e
SomeoneUnknownPerson <sema.shamaev1@gmail.com>
2022-02-17 10:38:21 +0900

    #18 Добавлены переменные email и password

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,7 +1,10 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {KeyboardAvoidingView, TextInput, TouchableOpacity, StyleSheet, Text, View} from 'react-native'
 
 const LoginScreen = () => {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
   return (
     <KeyboardAvoidingView
       style={styles.container}
@@ -10,10 +13,14 @@ const LoginScreen = () => {
       <View style={styles.inputContainer}>
         <TextInput
           placeholder="Email"
+          value={email}
+          onChangeText={text => setEmail(text)}
           style={styles.input}
         />
         <TextInput
           placeholder="Password"
+          value={password}
+          onChangeText={text => setPassword(text)}
           style={styles.input}
           secureTextEntry
         />

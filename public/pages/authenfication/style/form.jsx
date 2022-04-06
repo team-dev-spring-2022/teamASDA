b80d336e89f07950914ab38d7b7cf76815727d41
aKmiron <a.sofronova046a@mail.ru>
2022-04-06 16:30:28 +0900

    #27 Сверстка интерфейса на основе дизайн-макета

--- public/pages/authenfication/style/form.jsx
+++ public/pages/authenfication/style/form.jsx
@@ -2,5 +2,28 @@ import { StyleSheet } from 'react-native'
 import { COLORS } from './../../../templates/style/colors'
 
 export const styles = StyleSheet.create({
-    
+    container: {
+        padding: 32,
+    },
+    header: {
+        height: 120,
+        backgroundColor: COLORS().button,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    input: {
+        height: 40,
+        color: COLORS().defaultText,
+        fontSize: 16
+    },
+    footer: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        marginTop: 16
+    },
+    title: {
+        fontSize: 20,
+        color: COLORS().title,
+        fontWeight: '700'
+    }
 })

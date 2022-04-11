00f3f1850db13c0c170111d2db8c34185835d8e8
aKmiron <a.sofronova046a@mail.ru>
2022-04-11 11:38:52 +0900

    #18 регистрация и авторизация через api

--- public/pages/authenfication/Authenfication.jsx
+++ public/pages/authenfication/Authenfication.jsx
@@ -1,19 +1,26 @@
 import React from 'react'
 import { View, Text } from 'react-native'
+import { useSelector } from 'react-redux'
 
 import { styles } from './style/form'
 
 import Registration from './components/Registration'
+import Login from './components/Login'
 
 function Authenfication() {
+    const authScreen = useSelector(state => state.authScreen)
+
     return (
-        <>
+        <View>
             <View style={styles.header}>
                 <Text style={styles.title}>Authorization</Text>
             </View>
-            <Registration />
-
-        </>
+            {
+                authScreen == 1 
+                ? <Login />
+                : <Registration />
+            }
+        </View>
     )
 }
 

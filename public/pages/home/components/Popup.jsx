79c1a82d836b50fadd85b44ca7a5e7d30431d613
SomeoneUnknownPerson <sema.shamaev1@gmail.com>
2022-04-12 10:39:43 +0900

    #4O Сохранение задач пользователей

--- public/pages/home/components/Popup.jsx
+++ public/pages/home/components/Popup.jsx
@@ -1,11 +1,13 @@
 import React, { useState } from 'react'
-import { View, TouchableHighlight, Text, TextInput } from 'react-native'
+import { View, TouchableHighlight, Text, TextInput, Button as DefultButton, Alert } from 'react-native'
 
 import Add from './../svg/Add'
 import BackButton from './../svg/BackButton'
 
 import { COLORS } from './../../../templates/style/colors'
 import { styles } from './../style/popup'
+import { postTask } from '../../../api/postTask'
+import AsyncStorage from '@react-native-async-storage/async-storage'
 
 function Popup() {
     const [isOpen, setIsOpen] = React.useState(false)
@@ -37,6 +39,13 @@ function Popup() {
         const [title, setTitle] = useState('')
         const [time, setTime] = useState('')
 
+        async function sendTask() {
+            const token = await AsyncStorage.getItem('token')
+            await postTask(title, time, token).then(res => {
+                Alert.alert('Info', res.message)
+            })
+        }
+
         return (
             <View style={styles.container}>
                 <View style={styles.header}>
@@ -64,6 +73,12 @@ function Popup() {
                         onChangeText={setTime}
                         value={time}
                         placeholder='Task Time'
+                    />
+
+                    <DefultButton
+                        onPress={() => sendTask()}
+                        title='Create task'
+                        color={COLORS().button}
                     />
                 </View>
             </View>
